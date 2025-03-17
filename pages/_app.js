73600a136e26b40d49e9a381048a25cd3f989cd1
sql <teshyfraud@gmail.com>
2025-03-17 11:31:10 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ export default function MyApp({ Component, pageProps }) {
         <>
             <Head>
                 <title>RapScout</title>
-                <link rel="icon" href="/assets/favicon.png" />
+                <link rel="shortcut icon" href="/assets/favicon.png" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta name="description" content="RapScout - Track and analyze collectibles on Hexagon!" />
             </Head>
