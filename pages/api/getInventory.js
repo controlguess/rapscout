5f85b81dec71c9d8951dba50436537156c45837e
sql <teshyfraud@gmail.com>
2025-03-17 12:08:20 -0400

    Update getInventory.js

--- pages/api/getInventory.js
+++ pages/api/getInventory.js
@@ -9,26 +9,44 @@ export default async function handler(req, res) {
         return res.status(400).json({ error: 'Invalid User ID' });
     }
 
+    let fullInventory = [];
+    let currentPage = 1;
+    let nextPageCursor = null;
+
     try {
-        const response = await fetch('https://hexagon.pw/api/users/inventory', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                userid: parseInt(userid),
-                page: 1,
-            }),
-        });
 
-        if (!response.ok) {
-            throw new Error('Failed to fetch inventory data from Hexagon API');
-        }
+        do {
+            const response = await fetch('https://hexagon.pw/api/users/inventory', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    userid: parseInt(userid),
+                    page: currentPage,
+                }),
+            });
 
-        const data = await response.json();
+            if (!response.ok) {
+                throw new Error('Failed to fetch inventory data from Hexagon API');
+            }
 
-        if (data.success) {
-            res.status(200).json(data.data);
+            const data = await response.json();
+
+            if (data.success && data.data.inventory) {
+                fullInventory = [...fullInventory, ...data.data.inventory]; 
+            } else {
+                break; 
+            }
+
+            nextPageCursor = data.data.nextPageCursor;
+
+            currentPage++;
+
+        } while (nextPageCursor && !isNaN(nextPageCursor));
+
+        if (fullInventory.length > 0) {
+            res.status(200).json({ inventory: fullInventory });
         } else {
             res.status(404).json({ error: 'No inventory found for this user.' });
         }
